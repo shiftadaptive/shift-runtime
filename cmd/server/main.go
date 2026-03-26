58362034b1878dda097ba90dbda9adb70a176f8a
thev1ndu <itsthw9@gmail.com>
2026-03-26 16:52:32 +0530

    chore: enabled all origins

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,9 +9,10 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/joho/godotenv"
 	"shift/internal/handler"
 	"shift/pkg/logger"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -25,6 +26,15 @@ func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
 		if r.Method != http.MethodPost {
 			http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
 			return
